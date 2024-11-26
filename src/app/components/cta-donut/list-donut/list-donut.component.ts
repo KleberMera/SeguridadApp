70069ad7140fca2f430a210cfc69f104ac203227
KleberMera <klebermera2016@gmail.com>
2024-11-26 17:54:41 -0500

    getPorcentajeProtocolos for json

--- src/app/components/cta-donut/list-donut/list-donut.component.ts
+++ src/app/components/cta-donut/list-donut/list-donut.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { RestService } from '../../../services/rest.service';
+import { JsonService } from '../../../services/json.service';
 
 @Component({
   selector: 'app-list-donut',
@@ -10,13 +11,14 @@ import { RestService } from '../../../services/rest.service';
 export class ListDonutComponent {
   public listDonut = signal<any>([]);
   public readonly restService = inject(RestService);
+  public readonly jsonService = inject(JsonService);
   ngOnInit(): void {
     this.getPorcentajeProtocolos();
   }
 
   async getPorcentajeProtocolos() {
     try {
-      const res = await this.restService.getPorcentajeProtocolos().toPromise();
+      const res = await this.jsonService.getPorcentajeProtocolos().toPromise();
       this.listDonut.set(res);
     } catch (error) {}
   }
