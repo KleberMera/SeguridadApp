eef0c462043ec42f19ac8736ce4b1e56097083ad
KleberMera <klebermera2016@gmail.com>
2024-11-26 20:31:01 -0500

    donuts style

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './data.component.scss'
 })
 export class DataComponent {
+  protocols = [
+    {
+      name: 'QUIC',
+      description: 'Promedio de <strong>1007.93 ms</strong> por paquete con desviación estándar de <strong>523.05</strong>.'
+    },
+    {
+      name: 'TCP',
+      description: 'Promedio de <strong>304.17 ms</strong>, siendo la base de múltiples protocolos modernos.'
+    },
+    {
+      name: 'TLSv1.3',
+      description: 'Uso creciente con un <strong>8.55%</strong>, mejorando la seguridad y el rendimiento.'
+    },
+    {
+      name: 'HTTP',
+      description: 'Uso residual (<strong>0.02%</strong>), reflejando la transición hacia tecnologías avanzadas.'
+    }
+  ];
 
+  getProtocolDescription(protocol: { name: string, description: string }): string {
+    return `<strong>${protocol.name}: </strong>${protocol.description}`;
+  }
 }
